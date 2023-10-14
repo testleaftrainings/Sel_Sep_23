d161d7ab2821782c445d8ad8a532c9fc5277dd87
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2023-10-14 14:11:07 +0530

    Add files via upload

---
+++ week5/day1/testcase/CreateLead.java
@@ -0,0 +1,30 @@
+package testcase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+public class CreateLead extends BaseClass{
+	
+	@Test //mandatory annotation to execute the testcase
+	public  void runCreate() {		
+		System.out.println(Thread.currentThread().getId());
+		
+		driver.findElement(By.linkText("Creat Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.name("submitButton")).click();
+		
+}
+}
+
+
+
+
+
+
